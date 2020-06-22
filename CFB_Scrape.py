cb90e3a643e5da38cf148fb58598c9fb1bd7ebdb
Timothy Wu <wutimoth@usc.edu>
2020-06-22 01:26:39 -0700

    Update CFB_Scrape.py
    Works on every year on every category
    
    Will write pseudocode to make it easier to understand

--- CFB_Scrape.py
+++ CFB_Scrape.py
@@ -10,6 +10,7 @@ import requests
 import csv
 import re
 from datetime import datetime
+import os
 
 '''
 Scrape the following stats:
@@ -28,46 +29,55 @@ Scrape the following stats:
     Opponent Redzone Conversions
 '''
 
-score_off = 'Score_Off.csv' 
-score_def = 'Score_Def.csv'
 score_header = ['Rank', 'Name', 'G', 'TD', 'FG', '1XP', '2XP', 'Safety', 'Points', 'Points/G']
-score_off_url = 'http://www.cfbstats.com/2019/leader/national/team/offense/split01/category09/sort01.html'
 
-rush_off = 'Rush_Off.csv'
-rush_def = 'Rush_Def.csv'
 rush_header = ['Rank', 'Name', 'G', 'Att', 'Yards', 'Avg', 'TD', 'Att/G', 'Yards/G']
 
-pass_off = 'Pass_Off.csv'
-pass_def = 'Pass_Def.csv'
 pass_header = ['Rank', 'Name', 'G', 'Att', 'Comp', 'Pct', 'Yards', 'Yards/Att', 'TD', 'Int', 'Rating', 'Att/G', 'Yards/G']
 
-penalties = 'Penalties.csv'
-opp_penalties = 'Opp_Penalties.csv'
 penalties_header = ['Rank', 'Name', 'G', 'Pen', 'Yards', 'Pen/G', 'Yards/G']
 
-turnover = 'Turnover.csv'
 turnover_header = ['Rank', 'Name', 'G', 'Fum. Gain', 'Int. Gain', 'Total Gain', 'Fum. Lost', 'Int. Lost', 'Total Lost', 'Margin', 'Margin/G']
 
-third_conversions = 'Third_Conversions.csv'
-opp_third_conversions = 'Opp_Third_Conversions.csv'
 third_conversions_header = ['Rank', 'Name', 'G', 'Attempts', 'Conversions', 'Conversion %']
 
-redzone = 'Redzone.csv'
-opp_redzone = 'Opp_Redzone.csv'
 redzone_header = ['Rank', 'Name', 'G', 'Attempts', 'Scores', 'Score %', 'TD', 'TD %', 'FG', 'FG %']
 
-url = 'http://www.cfbstats.com/2019/leader/national/team/offense/split15/category09/sort01.html'
-r = requests.get(url)
-data = r.text
-soup = BeautifulSoup(data, "html.parser")
-table = soup.find('table')
-all_rows = []
-for trs in table.find_all('tr'):
-    tds = trs.find_all('td')
-    row = [cell.text.strip() for cell in tds]
-    if len(row) != 0 and (len(all_rows) == 0 or all_rows[-1][0] != row[0]):
-        all_rows.append(row)
+months = {15: 'August-September', 16: 'October', 17: 'November', 18: 'December'}
+modes = {'score': ('09', score_header), 'rush': ('01', rush_header), 'pass': ('02', pass_header), 'turnover': ('12', turnover_header), 
+    'penalties': ('14', penalties_header), '3rd': ('25', third_conversions_header), 'red': ('27', redzone_header)}
+sides = ['offense', 'defense']
 
-df = pd.DataFrame(data=all_rows, columns = header)
-df.set_index(['Rank'], drop=True, inplace=True)
-print("Scoring Offense Done")
+
+def getData(key, year, month, side):
+    url = 'http://www.cfbstats.com/' + str(year) + '/leader/national/team/' + side + '/split' + str(month) + '/category' + modes[key][0] + '/sort01.html'
+    r = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data, "html.parser")
+    table = soup.find('table')
+    all_rows = []
+    for trs in table.find_all('tr'):
+        tds = trs.find_all('td')
+        row = [cell.text.strip() for cell in tds]
+        if row:
+            all_rows.append(row)
+    df = pd.DataFrame(data=all_rows, columns = modes[key][1])
+    df.set_index(['Rank'], drop=True, inplace=True)
+    df.to_csv('data-' + str(year) + '-' + months[month] + '-' + side + '-' + key + '.csv', encoding = 'utf-8')
+    print('data-' + str(year) + '-' + months[month] + '-' + side + '-' + key + '.csv done')
+
+os.getcwd()
+os.chdir('Data')
+
+for year in range(2009, 2020):
+    for key in modes:
+        for month in months:
+            if key == 'turnover':
+                getData(key, year, month, sides[0])              
+            else:
+                for side in sides:
+                    getData(key, year, month, side)
+
+# test the scraper with just one scrape
+#getData('score', 2019, 15, 'offense')
+
